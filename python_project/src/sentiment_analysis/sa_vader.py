8d9a53b497492b5787f40eba1e1eae94ec1948ee
quayjunwei <quayjunwei.outlookg.com>
2023-10-17 00:37:39 +0800

    updated docstrings

--- python_project/src/sentiment_analysis/sa_vader.py
+++ python_project/src/sentiment_analysis/sa_vader.py
@@ -1,8 +1,8 @@
-'''
+"""
 sa_vader.py - Module containing functions for performing sentiment analysis on given dataset.
 Created by Ashsyahid H, October 2023.
 Released intto the public domain.
-'''
+"""
 import nltk
 import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -18,101 +18,205 @@ from transformers import AutoTokenizer
 import urllib.request
 from transformers import AutoModelForSequenceClassification
 
-nltk.download('vader_lexicon')
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
+nltk.download("vader_lexicon")
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
 
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+classifier = pipeline(
+    task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None
+)
 
 
 class SentimentAnalyzer:
-
     def __init__(self, df, name):
-        '''This __init__ method takes in a Pandas DataFrame and hotel name to facilitate sentiment analysis.'''
+        """
+        This __init__ function initialises the sentiment analysis class with a Pandas DataFrame and hotel name.
+
+        This constructor initializes the sentiment analysis class with the following parameters:
+        - df (pandas.DataFrame): A Pandas DataFrame containing the data to be analysed.
+        - name (str): The name of the hotel to be analysed.
+
+        Additionally, it sets up an attribute dictionary for sentiment categories with default values.
+
+        Args:
+            df (pandas.DataFrame): The input Pandas DataFrame for sentiment analysis.
+            name (str): The name of the hotel.
+
+        Returns:
+            None
+        """
         self.df = df
         self.name = name
-        self.attribute = {"admiration": 2, "amusement": 2, "approval": 2, "caring": 2, "desire": 2, "excitement": 2,
-                          "gratitude": 2, "joy": 2, "love": 2, "optimism": 2, "pride": 2,
-                          "confusion": 1, "curiosity": 1, "realization": 1, "surprise": 1, "neutral": 1,
-                          "anger": 0, "annoyance": 0, "dissapointment": 0, "disapproval": 0, "disgust": 0,
-                          "embarrassment": 0, "fear": 0, "grief": 0, "nervousness": 0, "remorse": 0, "sadness": 0}
+        self.attribute = {
+            "admiration": 2,
+            "amusement": 2,
+            "approval": 2,
+            "caring": 2,
+            "desire": 2,
+            "excitement": 2,
+            "gratitude": 2,
+            "joy": 2,
+            "love": 2,
+            "optimism": 2,
+            "pride": 2,
+            "confusion": 1,
+            "curiosity": 1,
+            "realization": 1,
+            "surprise": 1,
+            "neutral": 1,
+            "anger": 0,
+            "annoyance": 0,
+            "dissapointment": 0,
+            "disapproval": 0,
+            "disgust": 0,
+            "embarrassment": 0,
+            "fear": 0,
+            "grief": 0,
+            "nervousness": 0,
+            "remorse": 0,
+            "sadness": 0,
+        }
 
     def categorize(self, score):
-        '''This method takes in an argument 'score' and returns the sentiment based on the categorized value.'''
+        """
+        This function categorises sentiment based on a given score.
+
+        This function categorizes sentiment based on a numeric score, typically derived from sentiment analysis.
+        It returns one of three categories: 'positive', 'negative', or 'neutral', based on score thresholds.
+
+        Args:
+            score (float): The numeric sentiment score to be categorised.
+
+        Returns:
+            str: The categorised sentiment, which can be 'positive', 'negative', or 'neutral'.
+        """
         if score > 0.25:
             # pos
-            return 'positive'
+            return "positive"
         elif score < -0.25:
             # neg
-            return 'negative'
+            return "negative"
         else:
             # neutral
-            return 'neutral'
+            return "neutral"
 
-    def preprocess(self,text):
-        new_text = [
-        ]
+    def preprocess(self, text):
+        """
+        This function changes the input text by replacing specific patterns.
+
+        This function takes in a text as an argument and performs preprocessing by replacing certain patterns:
+        - Replaces mentions starting with '@' (e.g., @user) with '@user'.
+        - Replaces URLs starting with 'http' with 'http'.
+
+        Args:
+            text (str): The input text to be preprocessed.
+
+        Returns:
+            str: The preprocessed text with specific patterns replaced.
+        """
+        new_text = []
         for t in text.split(" "):
-            t = '@user' if t.startswith('@') and len(t) > 1 else t
-            t = 'http' if t.startswith('http') else t
+            t = "@user" if t.startswith("@") and len(t) > 1 else t
+            t = "http" if t.startswith("http") else t
             new_text.append(t)
         return " ".join(new_text)
 
     def preprocess_text(self, text):
-        '''This method performs preprocessing on the input text.'''
+        """
+        This function performs text preprocessing on the input text.
+
+        This method performs preprocessing on the input text, which includes:
+        - Tokenization to lowercase.
+        - Removal of English stopwords.
+        - Lemmatization of tokens.
+
+        Args:
+            text (str): The input text to be preprocessed.
+
+        Returns:
+            str: The preprocessed text after tokenization, stopwords removal, and lemmatization.
+        """
         tokens = word_tokenize(text.lower())
-        filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+        filtered_tokens = [
+            token for token in tokens if token not in stopwords.words("english")
+        ]
         lemmatizer = WordNetLemmatizer()
         lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
-        processed_text = ' '.join(lemmatized_tokens)
+        processed_text = " ".join(lemmatized_tokens)
 
         return processed_text
 
     def analyze_sentiment(self):
-        '''This method performs sentiment analysis on the given Pandas DataFrame.'''
+        """
+        This function performs sentiment analysis on the given Pandas DataFrame.
+
+        This method analyzes the sentiment of comments in the input Pandas DataFrame.
+        It extracts relevant columns, preprocesses the text, and calculates sentiment scores.
+        The sentiment is categorized based on the compound score into categories such as positive, negative, or neutral.
+
+        Returns:
+            pandas.DataFrame: A DataFrame with additional columns for sentiment scores and categorized sentiment labels.
+        """
         self.df.columns = [x.lower() for x in self.df.columns]
-        df = self.df.filter(['name', 'average_rating', 'rating', 'comment_content', 'date'], axis=1)
+        df = self.df.filter(
+            ["name", "average_rating", "rating", "comment_content", "date"], axis=1
+        )
         df.dropna(inplace=True)
         # hotel_name variable depends on user input i.e choose what hotel to perform SA on
-        df = df[df.loc[:, ['name']].values == self.name]
+        df = df[df.loc[:, ["name"]].values == self.name]
         # preprocess data
-        df['comment_content'] = df['comment_content'].apply(self.preprocess_text)
+        df["comment_content"] = df["comment_content"].apply(self.preprocess_text)
         analyzer = SentimentIntensityAnalyzer()
-        df['scores'] = df['comment_content'].apply(lambda review: analyzer.polarity_scores(review))
-        df['compound_score'] = df['scores'].apply(lambda d: d['compound'])
-        df['sentiment'] = df['compound_score'].apply(self.categorize)
+        df["scores"] = df["comment_content"].apply(
+            lambda review: analyzer.polarity_scores(review)
+        )
+        df["compound_score"] = df["scores"].apply(lambda d: d["compound"])
+        df["sentiment"] = df["compound_score"].apply(self.categorize)
         return df
 
     def analyze_emotions(self):
-        '''This method performs sentiment analysis on emotions in given Pandas DataFrame.'''
+        """
+        This function performs sentiment analysis on emotions in the given Pandas DataFrame.
+
+        This function analyzes emotions in comments from the input DataFrame for a specific task related to sentiment analysis.
+        It categorizes comments based on the expressed emotions and assigns points accordingly. The resulting DataFrame contains
+        the name, identified emotions, calculated points, and original ratings for each comment.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the analysis results with columns 'name', 'emotions', 'points', and 'ratings'.
+        """
         d = {"name": [], "emotions": [], "points": [], "ratings": []}
         self.df.columns = [x.lower() for x in self.df.columns]
         df = self.df
-        df = df[df.loc[:, ['name']].values == self.name]
+        df = df[df.loc[:, ["name"]].values == self.name]
         attr = self.attribute
         for index, row in df.iloc[:].iterrows():
             cumulative_scores = {}
-            t = row['comment_content']
+            t = row["comment_content"]
             if len(t) > 501:
-                split_text = [t[i:i + 500] for i in range(0, len(t), 500)]
+                split_text = [t[i : i + 500] for i in range(0, len(t), 500)]
                 for i, segment in enumerate(split_text, 1):
                     results = classifier([segment])
                     for results in results:
                         for entry in results:
-                            label = entry['label']
-                            score = entry['score']
+                            label = entry["label"]
+                            score = entry["score"]
                             if label not in cumulative_scores:
                                 cumulative_scores[label] = 0
                             cumulative_scores[label] += score
                 label_scores = list(cumulative_scores.items())
                 # Sort the list based on the score in descending order
                 sorted_labels = sorted(label_scores, key=lambda x: x[1], reverse=True)
-                sorted_labels_as_dicts = [{'label': label, 'score': score} for label, score in sorted_labels]
+                sorted_labels_as_dicts = [
+                    {"label": label, "score": score} for label, score in sorted_labels
+                ]
             else:
                 results = classifier([t])
-                sorted_labels_as_dicts = sorted(results[0], key=lambda x: x['score'], reverse=True)
-            emotions = [entry['label'] for entry in sorted_labels_as_dicts[0:5]]
+                sorted_labels_as_dicts = sorted(
+                    results[0], key=lambda x: x["score"], reverse=True
+                )
+            emotions = [entry["label"] for entry in sorted_labels_as_dicts[0:5]]
             points = 0
             for i in emotions:
                 if i in attr:
@@ -120,37 +224,46 @@ class SentimentAnalyzer:
             points = math.ceil(points / 2)
             if points == 0:
                 points = 1
-            d['name'].append(row["name"])
-            d['emotions'].append(emotions)
-            d['points'].append(float(points))
-            d["ratings"].append(row['rating'])
+            d["name"].append(row["name"])
+            d["emotions"].append(emotions)
+            d["points"].append(float(points))
+            d["ratings"].append(row["rating"])
 
             df = pd.DataFrame.from_dict(d)
         return df
 
     def analyze_sarcasm(self):
-        task = 'irony'
+        """
+        This function analyzes sarcasm in comments using a pre-trained model.
+
+        This function analyzes sarcasm in comments from the input DataFrame for a specific task related to irony/sarcasm detection.
+        It uses a pre-trained model to classify comments as sarcastic or not based on a given threshold.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the classification results where 'IsSarcasm' column indicates if a comment is sarcastic or not.
+        """
+        task = "irony"
         MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
-        sarcasm = {'IsSarcasm': []}
+        sarcasm = {"IsSarcasm": []}
         tokenizer = AutoTokenizer.from_pretrained(MODEL)
         labels = []
         mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
         with urllib.request.urlopen(mapping_link) as f:
-            html = f.read().decode('utf-8').split("\n")
-            csvreader = csv.reader(html, delimiter='\t')
+            html = f.read().decode("utf-8").split("\n")
+            csvreader = csv.reader(html, delimiter="\t")
         labels = [row[1] for row in csvreader if len(row) > 1]
         # PT
         model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
         df = self.df
-        df = df[df.loc[:, ['name']].values == self.name]
-        sarcasm = {'IsSarcasm': []}
+        df = df[df.loc[:, ["name"]].values == self.name]
+        sarcasm = {"IsSarcasm": []}
 
         # Assuming you've initialized tokenizer and model
         for i, row in df.iterrows():
-            text = row['comment_content']
+            text = row["comment_content"]
             if len(text) > 500:
-                split_text = [text[i:i + 500] for i in range(0, len(text), 500)]
+                split_text = [text[i : i + 500] for i in range(0, len(text), 500)]
             else:
                 split_text = [text]
             total_sScore = 0
@@ -161,7 +274,7 @@ class SentimentAnalyzer:
                 segment = self.preprocess(segment)
 
                 # Model inference
-                encoded_input = tokenizer(segment, return_tensors='pt')
+                encoded_input = tokenizer(segment, return_tensors="pt")
                 output = model(**encoded_input)
                 scores = output[0][0].detach().numpy()
                 scores = softmax(scores)
@@ -175,10 +288,9 @@ class SentimentAnalyzer:
                 total_slScore += sl
 
             if total_slScore > total_sScore:
-                sarcasm['IsSarcasm'].append(ls)
+                sarcasm["IsSarcasm"].append(ls)
             else:
-                sarcasm['IsSarcasm'].append(l)
-
+                sarcasm["IsSarcasm"].append(l)
 
         sarcasmdf = pd.DataFrame.from_dict(sarcasm)
 
--- python_project/src/visualisation/eda.py
+++ python_project/src/visualisation/eda.py
@@ -9,20 +9,44 @@ import altair as alt
 
 class EDA:
     def __init__(self, df, name=None):
-        """This __init__ method takes in a Pandas DataFrame and an optional hotel name to facilitate sentiment analysis."""
+        """
+        This __init__ method initialises the EDA (Exploratory Data Analysis) class with a Pandas DataFrame and an optional hotel name.
+
+        Args:
+            df (pandas.DataFrame): A Pandas DataFrame containing the data to be analyzed.
+            name (str, optional): The name of the hotel to be analyzed. Defaults to None.
+
+        Returns:
+            None
+        """
         self.df = df
         self.name = name
 
     @classmethod
     def create_instance(cls, data, selected_name):
+        """
+        This class method creates an instance of the EDA class by filtering the DataFrame based on the selected hotel name.
+
+        Args:
+            data (pandas.DataFrame): The source DataFrame with hotel data.
+            selected_name (str): The name of the hotel to be analyzed.
+
+        Returns:
+            EDA: An instance of the EDA class with the filtered data.
+        """
         # Filter the DataFrame based on the selected name
         filtered_df = data[data["name"] == selected_name]
         eda = cls(filtered_df, selected_name)
         return eda
 
     def plot_review_count_by_rating(self):
+        """
+        This function plots the review count by rating for the selected hotel.
+
+        Returns:
+            None
+        """
         rating_counts = self.df["rating"].value_counts().sort_index()
-        # Create a bar chart with Plotly Express
         fig = px.bar(
             x=rating_counts.index,
             y=rating_counts.values,
@@ -30,10 +54,14 @@ class EDA:
         )
         fig.update_xaxes(tickangle=0)
         st.plotly_chart(fig, use_container_width=True)
-        # Customize the x-axis orientation
-        # Rotate x-axis labels by 45 degrees
 
     def plot_average_price_by_rating(self):
+        """
+        This function plots the average price by rating for the selected hotel.
+
+        Returns:
+            None
+        """
         average_price_by_rating = self.df.groupby("rating")["price"].mean()
         fig = px.bar(x=average_price_by_rating.index, y=average_price_by_rating.values)
         fig.update_layout(
@@ -44,29 +72,30 @@ class EDA:
         st.plotly_chart(fig, use_container_width=True)
 
     def plot_grouped_bar_chart_for_avg_categories(self):
-        # Select columns 'location', 'cleanliness', 'service', and 'value'
+        """
+        This function plots a grouped bar chart for the average ratings of selected categories for the selected hotel.
+
+        Returns:
+            None
+        """
         selected_columns = ["location", "cleanliness", "service", "value"]
 
         # Calculate the average of selected columns for each rating
         avg_ratings = self.df.groupby("rating")[selected_columns].mean()
 
-        # Melt the dataframe to create a long format for Plotly Express
         melted_df = avg_ratings.reset_index().melt(
             id_vars="rating", var_name="category", value_name="avg_score"
         )
 
-        # Create a stacked bar chart with Plotly Express
         fig = px.bar(
             melted_df,
             x="rating",
             y="avg_score",
             color="category",
             title="Average Ratings for Selected Categories for all hotels",
-            barmode="relative",  # Stacked bars
-            category_orders={
-                "rating": ["Low", "Medium", "High"]
-            },  # Specify the order of ratings
-            color_discrete_sequence=px.colors.qualitative.Set3,  # Use a distinct color palette
+            barmode="relative",
+            category_orders={"rating": ["Low", "Medium", "High"]},
+            color_discrete_sequence=px.colors.qualitative.Set3,
         )
 
         fig.update_layout(
@@ -74,10 +103,15 @@ class EDA:
             yaxis_title="Average Score",
         )
 
-        # Display the chart
         st.plotly_chart(fig, use_container_width=True)
 
     def plot_average_hotel_class_by_rating(self):
+        """
+        This function plots the average rating by hotel class for the selected hotel.
+
+        Returns:
+            None
+        """
         average_price_by_rating = self.df.groupby("hotel_class")["rating"].mean()
         fig = px.bar(x=average_price_by_rating.index, y=average_price_by_rating.values)
         fig.update_layout(
@@ -87,10 +121,13 @@ class EDA:
         )
         st.plotly_chart(fig, use_container_width=True)
 
-    # junhong
     def plot_amenities_by_rating(self):
-        # Split the "Amenities" column and count the amenities for each row
+        """
+        This function plots the relationship between amenities count and rating for the selected hotel.
 
+        Returns:
+            None
+        """
         # Split the "Amenities" column and count the amenities for each row
         self.df["amenities_list"] = self.df["amenities"].str.split(", ")
         self.df["amenities_count"] = self.df["amenities_list"].apply(len)
@@ -98,7 +135,7 @@ class EDA:
         grouped_df = self.df.groupby("name", as_index=False).agg(
             {"amenities_count": "first", "hotel_average_ratings": "first"}
         )
-        # Create a scatter plot using Plotly Express with color based on mean rating
+
         fig = px.scatter(
             grouped_df,
             x="name",
@@ -112,26 +149,36 @@ class EDA:
             },
             color_continuous_scale="Viridis",  # Choose a color scale
         )
-        # Display the plot
+
         fig.update_traces(marker=dict(size=6))
         st.plotly_chart(fig)
 
     def plot_amenities_by_rating_box_whisker(self):
+        """
+        This function plots a box and whisker plot for amenities count by rating for the selected hotel.
+
+        Returns:
+            None
+        """
         amenities_list = self.df["amenities"].str.split(", ")
         amenities_count = amenities_list.apply(len)
         ratings = self.df["hotel_average_ratings"]
-        df = self.df
 
-        # Create a box and whisker plot
         fig = px.box(
-            df, x=ratings, y=amenities_count, title="amenities count by rating"
+            self.df, x=ratings, y=amenities_count, title="amenities count by rating"
         )
         fig.update_layout(yaxis_title="Amenities Count", xaxis_title="Hotel Ratings")
         fig.update_xaxes(type="linear")
-        # Showing the plot
+
         st.plotly_chart(fig, use_container_width=True)
 
     def plot_near_attractions_restaurants(self):
+        """
+        This function plots the number of nearby restaurants and attractions for the selected hotel.
+
+        Returns:
+            None
+        """
         # Split the "near_attraction" column into separate columns for restaurants and attractions
         self.df["restaurants"] = (
             self.df["near_attractions"].str.extract(r"(\d+) Restaurants").astype(int)
@@ -140,7 +187,6 @@ class EDA:
             self.df["near_attractions"].str.extract(r"(\d+) Attractions").astype(int)
         )
 
-        # Create a scatter plot of rating vs. number of restaurants using Plotly Express
         fig = px.scatter(
             self.df,
             x="restaurants",
@@ -153,26 +199,28 @@ class EDA:
             },
         )
 
-        # Customize the color scale
         fig.update_traces(marker=dict(size=5))
 
         st.plotly_chart(fig, use_container_width=True)
 
     def plot_average_ratings_by_date(self):
+        """
+        This function plots the average ratings over years for the selected hotel.
+
+        Returns:
+            None
+        """
         # Extract the month and year from the "Date of stay" column
-        self.df["date"] = pd.to_datetime(
-            self.df["date"], format="Date of stay: %B %Y", errors="coerce"
-        )
-        self.df.dropna(
-            subset=["date"], inplace=True
-        )  # Remove rows with invalid date values
+        # self.df["date"] = pd.to_datetime(
+        #     self.df["date"], format="Date of stay: %B %Y", errors="coerce"
+        # )
+        # self.df.dropna(subset=["date"], inplace=True)
 
         # Group the data by year and calculate the average rating for each year
         yearly_ratings = (
             self.df.groupby(self.df["date"].dt.year)["rating"].mean().reset_index()
         )
 
-        # Create a line chart of average ratings over years
         fig = px.line(
             yearly_ratings,
             x="date",
@@ -184,11 +232,17 @@ class EDA:
         fig.update_xaxes(
             title_text="Year",
         )
-        fig.update_traces(mode="lines+markers")  # Add markers to the line chart
+        fig.update_traces(mode="lines+markers")
 
         st.plotly_chart(fig, use_container_width=True)
 
     def plot_near_attractions_restaurants3(self):
+        """
+        This function plots the relationship between the number of nearby restaurants, attractions, and average ratings for the selected hotel.
+
+        Returns:
+            None
+        """
         # Split the "near_attraction" column into separate columns for restaurants and attractions
         self.df["restaurants"] = (
             self.df["near_attractions"].str.extract(r"(\d+) Restaurants").astype(int)
@@ -242,7 +296,6 @@ class EDA:
             self.df.groupby("attractions_bin")["rating"].mean().reset_index()
         )
 
-        # Create line charts for average ratings vs. number of restaurants and attractions
         fig1 = px.line(
             average_ratings_restaurants,
             x="restaurants_bin",
@@ -265,7 +318,6 @@ class EDA:
             },
         )
 
-        # Display the two line charts side by side
         col1, col2 = st.columns(2)
         with col1:
             st.plotly_chart(fig1)
